48322db0d1c39f7bbfa53a2cf240da0e4204d483
Manu Mathew <a0393608@udtensorlab5>
2021-11-08 22:54:08 +0530

    setup update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ echo "mmdetection is not installing cython properly - it has to be installed fro
 conda install -y cython
 
 echo "Installing pytorch and torchvision"
-pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio===0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install torch==1.9.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
 
 
 echo "Installing mmdetection"
